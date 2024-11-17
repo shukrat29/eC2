7b9d9de42e9a3e9d606350ba4311356fcade9dfa
Shukra Tamang <shukratamang.npl@gmail.com>
2024-11-17 18:44:30 +1100

    set up tailwindcss

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
 import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("You are getting this message from server");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Server running successfully on port ${port}`);
+});
